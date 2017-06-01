7cf66b52552195d54a59df0a819e667e434c2ad1
Robert Bunch <rob@digitalcrafts.com>
2017-06-01 11:42:34 -0400

    Add form

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ var config = require('../config/config')
 
 console.log(config);
 
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+config.apiKey
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	const apiBaseUrl = 'http://api.themoviedb.org/3';
-	const nowPlayingUrl = apiBaseUrl + '/movie/now_playing?api_key='+apiKey
-	const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
-
 	request.get(nowPlayingUrl,(error,response,movieData)=>{
 		var movieData = JSON.parse(movieData);
 		console.log(movieData)
@@ -19,8 +20,24 @@ router.get('/', function(req, res, next) {
 			imageBaseUrl: imageBaseUrl
 		});
 	});
-
-  
 });
+
+// router.get('/search',(req, res)=>{
+// 	res.send("The get search page");
+// });
+
+router.post('/search',(req, res)=>{
+	// req.body is available because of the body-parser moduel.
+	// body-parser module was installed when you created teh expressaapp
+	// req.bdoy is where POSTED data will live.
+	// res.json(req.body);
+	var termUserSearchedFor = req.body.searchString;
+	var searchUrl = apiBaseUrl + '/search/movie?query='+termUserSearchedFor+'&api_key='+config.apiKey;
+	request.get(searchUrl,(error,response,movieData)=>{
+		res.json(JSON.parse(movieData));
+	});
+	// res.send("The post search page");
+});
+
 
 module.exports = router;
